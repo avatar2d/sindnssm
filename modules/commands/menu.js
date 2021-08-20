cf16e3422c0aa2c6f5225109ce900199d01e9af0
avatar2d <86334386+avatar2d@users.noreply.github.com>
2021-08-20 17:53:01 +0700

    Update menu.js

--- modules/commands/menu.js
+++ modules/commands/menu.js
@@ -16,14 +16,14 @@ module.exports.config = {
 module.exports.languages = {
 	"vi": {
 		"moduleInfo": "「 %1 」\n%2\n\n❯ Cách sử dụng: %3\n❯ Thuộc nhóm: %4\n❯ Thời gian chờ: %5 giây(s)\n❯ Quyền hạn: %6\n\n» Module code by %7 «",
-		"helpList": '[ Hiện tại đang có %1 lệnh có thể sử dụng trên bot này, Sử dụng: "%2help nameCommand" để xem chi tiết cách sử dụng! | Fb Admin BoT: https://www.fb.com/manhict ]"',
+		"helpList": '[ Hiện tại đang có %1 lệnh có thể sử dụng trên bot này, Sử dụng: "%2help nameCommand" để xem chi tiết cách sử dụng! ]"',
 		"user": "Người dùng",
         "adminGroup": "Quản trị viên nhóm",
         "adminBot": "Quản trị viên bot"
 	},
 	"en": {
 		"moduleInfo": "「 %1 」\n%2\n\n❯ Usage: %3\n❯ Category: %4\n❯ Waiting time: %5 seconds(s)\n❯ Permission: %6\n\n» Module code by %7 «",
-		"helpList": '[ There are %1 commands on this bot, Use: "%2help nameCommand" to know how to use! | Fb Admin BoT: https://www.fb.com/manhict ]',
+		"helpList": '[ There are %1 commands on this bot, Use: "%2help nameCommand" to know how to use! |  ]',
 		"user": "User",
         "adminGroup": "Admin group",
         "adminBot": "Admin bot"
